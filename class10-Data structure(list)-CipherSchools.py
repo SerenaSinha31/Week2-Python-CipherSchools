abd6e5465b346f0c101d8c332608b4457308ea6d
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-12-12 15:24:56 +0530

    Add files via upload

---
+++ class10-Data structure(list)-CipherSchools.py
@@ -0,0 +1,19 @@
+#LIST
+
+#Lists are ordered collection of items
+
+numbers=[1,2,3,4,5]
+print(numbers[:1])
+
+words=['word1','word2','word3']
+print(words[:2])
+
+
+#Add items using index
+mixed=[1,2,3,4,5,23,None]
+mixed[1]='two'
+#print(mixed)
+mixed[1:]='three'
+mixed[2:]='five'
+print(mixed)
+
