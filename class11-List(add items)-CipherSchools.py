352a7e9989f97ccb91e38af6afe7aa1db77cb50c
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-12-12 15:25:09 +0530

    Add files via upload

---
+++ class11-List(add items)-CipherSchools.py
@@ -0,0 +1,29 @@
+fruits=["grapes","Appples"]
+fruits.append("Mango")
+print(fruits)
+
+fruits=[ ]
+fruits.append("Mango")
+fruits.append("Bannana")
+print(fruits)
+
+#More methods to add in python
+fruits=["Mango","Orange"]
+fruits1=["Mango","Orange"]
+fruits2=["Mango","Orange"]
+
+fruitstotal=fruits+fruits1+fruits2
+print(fruitstotal)
+
+#using insert method
+fruits.insert(1,"Apples")
+print(fruits)
+
+#extend method
+fruits1.extend(fruits2)
+print(fruits1)
+print(fruits2)
+
+
+
+
